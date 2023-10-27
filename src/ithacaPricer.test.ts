a98627f5d1152118afcc733e5bfa1644904944b0
nhaga <nhaga5@gmail.com>
2023-10-27 01:12:04 +0100

    initial commit

---
+++ src/ithacaPricer.test.ts
@@ -0,0 +1,316 @@
+import { describe, expect, it, beforeEach } from "vitest";
+
+
+import { BlackScholes, Option } from "./ithacaPricer";
+
+let blackScholes: BlackScholes;
+let option: Option;
+let precision: number;
+let sigma: number;
+
+describe("option", () => {
+    blackScholes = new BlackScholes();
+    precision = 5;
+  
+    describe("normal prices", () => {
+      // online calculator
+      // https://www.math.drexel.edu/~pg/fin/VanillaCalculator.html
+  
+
+
+
+      
+      it("call", () => {
+          const option = blackScholes.option({
+            rate: 0.00,
+            sigma: 0.485,
+            strike: 1850,
+            time: 0.0739726,
+            type: "call",
+            underlying: 1805,
+          });
+        expect(option.price).toBeCloseTo(75.268, precision);
+      });
+  
+      it("put", () => {
+        const option = blackScholes.option({
+            rate: 0.00,
+            sigma: 0.485,
+            strike: 1850,
+            time: 0.0739726,
+            type: "put",
+            underlying: 1805,
+          });
+        expect(option.price).toBeCloseTo(120.2684, precision);
+      });
+    });
+
+    describe("greeks", () => {
+        option = blackScholes.option({
+            rate: 0.0,
+            sigma: 0.485,
+            strike: 1850,
+            time: 0.07397,
+            type: "call",
+            underlying: 1805,
+          });
+        it("delta", () => {
+              expect(option.delta).toBeCloseTo(0.45195, precision);
+        })
+        it("gamma", () => {
+            expect(option.gamma).toBeCloseTo(0.0016, precision);
+        })
+        it("theta", () => {
+            expect(option.theta).toBeCloseTo(637.391, precision);
+        })
+        it("vega", () => {
+            expect(option.vega).toBeCloseTo(194.424047, precision);
+        })
+
+
+
+    })
+    describe("corner cases", () => {
+        // credits: https://github.com/MattL922/black-scholes/blob/master/black-scholes.js
+    
+        describe("t>0, v>0", () => {
+          it("call", () => {
+            option = blackScholes.option({
+              rate: 0.08,
+              sigma: 0.2,
+              strike: 34,
+              time: 0.25,
+              type: "call",
+              underlying: 30,
+            });
+            expect(option.price).toBeCloseTo(0.23834902311961947, precision);
+          });
+    
+          it("put", () => {
+            option = blackScholes.option({
+              rate: 0.08,
+              sigma: 0.2,
+              strike: 34,
+              time: 0.25,
+              type: "put",
+              underlying: 30,
+            });
+            expect(option.price).toBeCloseTo(3.5651039155492974, precision);
+          });
+        });
+    
+        describe("t>0, v=0, out-of-the-money", () => {
+          it("call", () => {
+            option = blackScholes.option({
+              rate: 0.08,
+              sigma: 0,
+              strike: 34,
+              time: 0.25,
+              type: "call",
+              underlying: 30,
+            });
+            expect(option.price).toBe(0);
+          });
+    
+          it("put", () => {
+            option = blackScholes.option({
+              rate: 0.08,
+              sigma: 0,
+              strike: 34,
+              time: 0.25,
+              type: "put",
+              underlying: 35,
+            });
+            expect(option.price).toBe(0);
+          });
+        });
+    
+        describe("t=0, v>0, out-of-the-money", () => {
+          it("call", () => {
+            option = blackScholes.option({
+              rate: 0.08,
+              sigma: 0.1,
+              strike: 34,
+              time: 0,
+              type: "call",
+              underlying: 30,
+            });
+            expect(option.price).toBe(0);
+          });
+    
+          it("put", () => {
+            // underlying, strike, time, sigma, rate, type
+            option = blackScholes.option({
+              rate: 0.08,
+              sigma: 0.1,
+              strike: 34,
+              time: 0,
+              type: "put",
+              underlying: 35,
+            });
+            expect(option.price).toBe(0);
+          });
+        });
+    
+        describe("t=0, v=0, out-of-the-money", () => {
+          it("call", () => {
+            option = blackScholes.option({
+              rate: 0.08,
+              sigma: 0,
+              strike: 34,
+              time: 0,
+              type: "call",
+              underlying: 30,
+            });
+            expect(option.price).toBe(0);
+          });
+    
+          it("put", () => {
+            option = blackScholes.option({
+              rate: 0.08,
+              sigma: 0,
+              strike: 34,
+              time: 0,
+              type: "put",
+              underlying: 35,
+            });
+            expect(option.price).toBe(0);
+          });
+        });
+    
+        describe("t>0, v=0, in-the-money", () => {
+    
+          it("call", () => {
+            option = blackScholes.option({
+              rate: 0.08,
+              sigma: 0,
+              strike: 34,
+              time: 0.25,
+              type: "call",
+              underlying: 36,
+            });
+            expect(option.price).toBeCloseTo(2.673245107570324, precision);
+          });
+    
+          it("put", () => {
+            option = blackScholes.option({
+              rate: 0.08,
+              sigma: 0,
+              strike: 34,
+              time: 0.25,
+              type: "put",
+              underlying: 32,
+            });
+            expect(option.price).toBeCloseTo(1.3267548924296761, precision);
+          });
+        });
+    
+        describe("t=0, v>0, in-the-money", () => {
+          it("call", () => {
+            option = blackScholes.option({
+              rate: 0.08,
+              sigma: 0.1,
+              strike: 34,
+              time: 0,
+              type: "call",
+              underlying: 36,
+            });
+            expect(option.price).toBe(2);
+          });
+    
+          it("put", () => {
+            option = blackScholes.option({
+              rate: 0.08,
+              sigma: 0.1,
+              strike: 34,
+              time: 0,
+              type: "put",
+              underlying: 32,
+            });
+            expect(option.price).toBe(2);
+          });
+        });
+    
+        describe("t=0, v=0, in-the-money", () => {
+          it("call", () => {
+            option = blackScholes.option({
+              rate: 0.08,
+              sigma: 0,
+              strike: 34,
+              time: 0,
+              type: "call",
+              underlying: 36,
+            });
+            expect(option.price).toBe(2);
+          });
+    
+          it("put", () => {
+            option = blackScholes.option({
+              rate: 0.08,
+              sigma: 0,
+              strike: 34,
+              time: 0,
+              type: "put",
+              underlying: 32,
+            });
+            expect(option.price).toBe(2);
+          });
+        });
+      });
+    
+})
+
+describe("sigma", () => {
+    precision = 3;
+  
+    const rate: number = 0.1;
+    const strike: number = 90;
+    const time: number = 0.5;
+    const type: "call" | "put" = "call";
+    const underlying: number = 100;
+  
+  
+    it("newton-raphson", () => {
+      blackScholes = new BlackScholes();
+  
+      option = blackScholes.option({
+        rate,
+        sigma: 0.8,
+        strike,
+        time,
+        type,
+        underlying,
+      });
+      expect(option.price).toBeCloseTo(28.61495, precision);
+  
+      sigma = blackScholes.sigma({
+        price: option.price,
+        rate,
+        strike,
+        time,
+        type,
+        underlying,
+      });
+      expect(sigma).toBeCloseTo(0.8, precision);
+    });
+
+    it("newton-raphson, zero sigma", () => {
+        blackScholes = new BlackScholes({
+            priceToSigmaAccuracy: 20
+          
+        });
+    
+        sigma = blackScholes.sigma({
+          price: 1000,
+          rate,
+          strike,
+          time,
+          type,
+          underlying,
+        });
+        expect(sigma).toBeCloseTo(0.0, precision);
+      });
+  
+  
+  });
+  
---
+++ src/ithacaPricer.ts
@@ -0,0 +1,137 @@
+type TypeBoolean = boolean // true for call, false for put
+type TypeUnion = 'call' | 'put'
+type Underlying = Price // current price of underlying
+type Vega = number // dPrice/dSigma → dSigma +1 means +100%
+
+export interface Option {
+    price: Price
+    delta: Delta
+    gamma: Gamma
+    theta: Theta
+    vega: Vega
+  }
+
+  export class BlackScholes {
+
+    public constructor(args?: {
+        priceToSigmaAccuracy?: number
+        priceToSigmaBLeft?: number
+        priceToSigmaBRight?: number
+        priceToSigmaNRIteractions?: number
+      }) {      
+        this.priceToSigmaAccuracy = args?.priceToSigmaAccuracy ?? 0.001
+        this.priceToSigmaBLeft = args?.priceToSigmaBLeft ?? 0
+        this.priceToSigmaBRight = args?.priceToSigmaBRight ?? 2
+        this.priceToSigmaNRIterations = args?.priceToSigmaNRIteractions ?? 10
+      }
+
+  // Cumulative normal distribution function (interpolated, single precision accuracy)
+  private CNDF(this: void, x: number): number {
+    const a1 = 0.31938153
+    const a2 = -0.356563782
+    const a3 = 1.781477937
+    const a4 = -1.821255978
+    const a5 = 1.330274429
+
+    let k1: number
+    let k2: number
+
+    if (x < 0) return 1.0 - this.CNDF(-x)
+    else if (x > 6) return 1.0
+    else {
+      k1 = 1.0 / (1.0 + 0.2316419 * x)
+      k2 = ((((a5 * k1 + a4) * k1 + a3) * k1 + a2) * k1 + a1) * k1
+      return 1.0 - BlackScholes.NDF(x) * k2
+    }
+  }
+
+      public option(args: {
+        rate: Rate
+        sigma: Sigma
+        strike: Strike
+        time: Time
+        type: TypeUnion
+        underlying: Underlying
+      }): Option {
+        const { rate, sigma, strike, time, type, underlying } = args
+        return this.sigmaToPrice(rate, sigma, strike, time, type === 'call', underlying)
+      }
+
+      public sigma(args: {
+        price: Price
+        rate: Rate
+        strike: Strike
+        time: Time
+        type: TypeUnion
+        underlying: Underlying
+      }): Sigma {
+        const { price, rate, strike, time, type, underlying } = args
+        return this.priceToSigma(price, rate, strike, time, type === 'call', underlying)
+      }
+
+  // Calculates option params by given historical volatility
+  private sigmaToPrice(
+    rate: Rate,
+    sigma: Sigma,
+    strike: Strike,
+    time: Time,
+    type: TypeBoolean,
+    underlying: Underlying,
+  ): Option {
+    const sqrt_t = Math.sqrt(time)
+    const exp_rt = Math.exp(-rate * time)
+
+    const d1 =
+      (Math.log(underlying / strike) + rate * time) / (sigma * sqrt_t) + 0.5 * (sigma * sqrt_t)
+    const d2 = d1 - sigma * sqrt_t
+
+    const CNDF_d1 = type ? this.CNDF(d1) : -this.CNDF(-d1)
+    const CNDF_d2 = type ? this.CNDF(d2) : -this.CNDF(-d2)
+    const NDF_d1 = BlackScholes.NDF(d1)
+
+    return {
+      delta: CNDF_d1,
+      gamma: NDF_d1 / (underlying * sigma * sqrt_t),
+      price: underlying * CNDF_d1 - strike * exp_rt * CNDF_d2,
+      theta: (underlying * sigma * NDF_d1) / (2 * sqrt_t) + rate * strike * exp_rt * CNDF_d2,
+      vega: underlying * sqrt_t * NDF_d1,
+    }
+  }
+
+    // Implied volatility (Newton-Raphson method)
+  private priceToSigma(
+    price: Price,
+    rate: Rate,
+    strike: Strike,
+    time: Time,
+    type: TypeBoolean,
+    underlying: Underlying,
+  ): Sigma {
+    const ACCURACY = this.priceToSigmaAccuracy
+    const ITERATIONS = this.priceToSigmaNRIterations
+
+    // initial guess
+
+    let sigma: Sigma = (Math.sqrt((2 * Math.PI) / time) * price) / underlying
+
+    // iterate
+
+    let option: Option
+    let dprice: Price
+
+    for (let i = 0; i < ITERATIONS; i++) {
+      option = this.sigmaToPrice(rate, sigma, strike, time, type, underlying)
+      dprice = option.price - price
+
+      if (Math.abs(dprice) < ACCURACY) return sigma
+      else if (option.vega === 0) return 0
+      else sigma -= dprice / option.vega
+    }
+  }  
+
+    // Normal distribution function
+    private static NDF(this: void, x: number): number {
+        return Math.exp(-(x * x) / 2) / Math.sqrt(2 * Math.PI)
+    }
+  }
+
